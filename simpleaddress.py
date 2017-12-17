cfbe87b85834bca35e1ac44e85619b447db2c78d
Max Erickson <maxerickson@gmail.com>
2017-12-17 12:35:57 -0500

    Clean out regex code, rename replacer to normalize.

--- simpleaddress.py
+++ simpleaddress.py
@@ -40,37 +40,23 @@ for d in ["Sw","Se","Nw","Ne","Us"]:
     wordmap[d]=d.upper()
 
 replacemap=dict()
-rmap2=dict()
 for word,rep in wordmap.items():
-    regex=re.compile("(^|(?<=\s))"+re.escape(word)+"(?=\s|$)", re.I)
-    replacemap[regex]=rep
-    rmap2[word.lower()]=rep
+    replacemap[word.lower()]=rep
 
 numbersuffixes=["Nd","Rd","St","Th"]
-# check for number suffixes with different capitalization style than OSM
-for suffix in ["Nd","Rd","St","Th"]:
-    regex=re.compile("\d"+suffix+"(?=\s|$)",re.I)
-    replacemap[regex]=suffix.lower()
-    #~ rmap2[suffix.lower()]=suffix.lower()
 
-def replacer(address):
-    for word,rep in replacemap.items():
-        address=word.sub(rep, address)
-    return address
-
-def replacer2(address):
+def normalize(address):
     parts=address.split(" ")
     for i,part in enumerate(parts):
-        if part.lower() in rmap2:
-            parts[i]=rmap2[part.lower()]
+        if part.lower() in replacemap:
+            parts[i]=replacemap[part.lower()]
         for s in numbersuffixes:
             if s in part and part[0].isnumeric():
                 parts[i]=part.replace(s,s.lower())
     return " ".join(parts)
-replacer=replacer2
-print(rmap2)
+
 def compare(addr1,addr2):
-    return replacer(addr1)==replacer(addr2)
+    return normalize(addr1)==normalize(addr2)
 
 units={
 "Suite":"Ste",
@@ -83,16 +69,18 @@ def hasunit(addr):
             return True
 
 if __name__=="__main__":
-    print(replacer("601 North Lincoln Road"))
-    print(replacer("601 N Lincoln Road"))
-    print(replacer("601 N. Lincoln Rd"))
-    print(replacer("601 north Lincoln road"))
-    print(replacer("7600 Sw Dartmouth St."))
-    print(replacer("7600 Southwest Dartmouth Street"))
-    print(replacer("1203 6Th Ave Se"))
-    print(replacer("1203 6th Avenue Southeast"))
-    print(replacer("16746 E Smoky Hill Rd"))
-    print(replacer("16746 East Smoky Hill Road"))
+    print(normalize("601 North Lincoln Road"))
+    print(normalize("601 N Lincoln Road"))
+    print(normalize("601 N. Lincoln Rd"))
+    print(normalize("601 north Lincoln road"))
+    print(normalize("7600 Sw Dartmouth St."))
+    print(normalize("7600 Southwest Dartmouth Street"))
+    print(normalize("1203 6Th Ave Se"))
+    print(normalize("1203 6th Avenue Southeast"))
+    print(normalize("16746 E Smoky Hill Rd"))
+    print(normalize("16746 East Smoky Hill Road"))
+    print(normalize("8270 Delta Shores Circle"))
+    print(normalize("8270  Delta Shores Circle"))
     addrhousenumber="601"
     addrstreet="North Lincoln Road"
     addrfull="601 N Lincoln Road"
