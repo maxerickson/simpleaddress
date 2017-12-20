ed0db853e4426c3a4246afc6acb0383c93e766eb
Max Erickson <maxerickson@gmail.com>
2017-12-20 17:08:15 -0500

    Finish cleanup of regex code, add a usage example.

---
+++ example1.py
@@ -0,0 +1,24 @@
+import simpleaddress
+
+def parts(address):
+    number, street=address.split(" ",1)
+    unit=""
+    for test in [" Suite "," Unit "]:
+        if test in street:
+            street,ste,uni=street.partition(test)
+            unit=ste.lstrip()+uni
+            street=street.strip(",")
+    return number, street, unit
+
+addresses="""3-3300 Kuhio Hwy
+11400 Highway 99
+2909 Austell Rd Sw Suite 100
+1334 Flammang Dr""".splitlines()
+
+for address in addresses:
+	normal=simpleaddress.normalize(address)
+	#~ print(address, normal)
+	expand=simpleaddress.expand_streetname(normal)
+	#~ print(address, expand)
+	number, street, unit=parts(expand)
+	print(number, street, unit, sep="|")
--- simpleaddress.py
+++ simpleaddress.py
@@ -1,9 +1,8 @@
 """
 Compare addresses by shortening them on word boundaries.
 """
-import re
 
-wordmap={
+directions={
 "North":"N",
 "South":"S",
 "East":"E",
@@ -11,7 +10,10 @@ wordmap={
 "Northwest":"NW",
 "Northeast":"NE",
 "Southwest":"SW",
-"Southeast":"SE",
+"Southeast":"SE"
+}
+
+street_suffixes={
 "Avenue":"Ave",
 "Boulevard":"Blvd",
 "Bypass":"Byp",
@@ -20,43 +22,76 @@ wordmap={
 "Court":"Ct",
 "Drive":"Dr",
 "Expressway":"Expy",
+"Freeway":"Fwy",
 "Gateway":"Gtwy",
 "Highway":"Hwy",
 "Lane":"Ln",
 "Parkway":"Pkwy",
 "Place":"Pl",
+"Plaza":"Plz",
 "Road":"Rd",
 "Route":"Rt",
 "Square":"Sq",
 "Street":"St",
+"Suite":"Ste",
 "Terrace":"Ter",
 "Trail":"Trl"
 }
+
+saints=["Agnes","Ann","Anne","Charles","Clair","Elizabeth",
+            "Francis","George","Mark","Mary"]
+saints=set(saints)
+
+wordmap=dict()
+wordmap.update(directions)
+wordmap.update(street_suffixes)
 # check for abbreviations followed by a period
 for d in list(wordmap.values()):
-    wordmap[d+"."]=d
+    wordmap[(d+".").lower()]=d
+
 # check for abbreviations with different capitalization style than OSM
 for d in ["Sw","Se","Nw","Ne","Us"]:
     wordmap[d]=d.upper()
 
-replacemap=dict()
-for word,rep in wordmap.items():
-    replacemap[word.lower()]=rep
-
+# normalize keys to lowercase
+replacemap=dict((word.lower(),rep) for word,rep in wordmap.items())
 numbersuffixes=["Nd","Rd","St","Th"]
 
 def normalize(address):
+    """Normalize individual words to titlecase, abbreviated versions."""
     parts=address.split(" ")
     for i,part in enumerate(parts):
         if part.lower() in replacemap:
             parts[i]=replacemap[part.lower()]
-        for s in numbersuffixes:
-            if s in part and part[0].isnumeric():
-                parts[i]=part.replace(s,s.lower())
+        if part and part[0].isnumeric():
+            for s in numbersuffixes:
+                if s in part:
+                    parts[i]=part.replace(s,s.lower())
     return " ".join(parts)
 
 def compare(addr1,addr2):
     return normalize(addr1)==normalize(addr2)
+
+def expand_saint(parts):
+    sainted=saints.intersection(parts)
+    for s in sainted:
+        i=parts.index(s)
+        if i > 0 and parts[i-1].lower() in {"st.","st"}:
+            parts[i-1]="Saint"
+    return parts
+
+revmap=dict((v,k) for k,v in street_suffixes.items())
+for k,v in directions.items():
+    revmap[v]=k
+def expand_streetname(name):
+    parts=name.split()
+    parts=expand_saint(parts)
+    for i in range(len(parts)):
+        if parts[i] in revmap:
+            parts[i]=revmap[parts[i]]
+    return " ".join(parts)
+            
+
 
 units={
 "Suite":"Ste",
@@ -86,3 +121,8 @@ if __name__=="__main__":
     addrfull="601 N Lincoln Road"
     osmaddress="{} {}".format(addrhousenumber,addrstreet)
     print(compare(osmaddress,addrfull))
+
+    print(expand_streetname("Mary Street"))
+    print(expand_streetname("St. Mary St"))
+    print(expand_streetname("St Mary Ave"))
+    print(expand_streetname("150 St Mark Court, Dallas"))
